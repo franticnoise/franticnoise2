8d508c0b7aca0a375944e0706a1767c91048c04f
franticnoise <111320557+franticnoise@users.noreply.github.com>
2022-09-05 11:28:46 -0600

    Add files via upload

---
+++ body_mobile.js
@@ -0,0 +1,19 @@
+document.getElementById('brody').innerHTML = 
+'<main><script type=text/javascript src=header_mobile.js></script>' + 
+'<script type=module src=script.js></script><script type=module src=footer.js></script>' + 
+'<div id=header_mobile></div>' + 
+'<iframe height=550 src=actualpresets.html width=800 class=mainiframe allowtransparency=true></iframe></div>' + 
+'<div id=footer class=footer></div>';
+ /*
+            
+            document.write('<script type=module src=menu.js>');
+            document.write('<div id=myNav class=wrapper></div>');
+            document.write('<div class=headermainiframecontainer>');*/ 
+
+
+   /*document.write('<script type=module src=footer.js>');
+              document.write('<script type=module src=header.js>');
+              document.write('<script type=module src=script.js>');
+              document.write('<script type=module src=menu_mobile.js>');
+              document.write('<div id=header_mobile></div>');
+              document.write('<div id=myNavMobile class=wrapper></div>');*/ 
---
+++ header_mobile.js
@@ -0,0 +1,8 @@
+document.getElementById('header_mobile').innerHTML =
+'<div class="headerparent" style="">' +
+'        <div class="headerchild" style="justify-content:center;border:0px solid #000000;width:220px;">' +
+'          <h3><img class="franticlogo" src="img/newlogoFrantic.png" width="120"/></h3>' +
+'         <h3 class="animate__animated animate__backInDown animate__delay-2s " style="font-size: 14px">Frantic Noise </h3>' +
+'          <h3 class="animate__animated animate__rubberBand animate__delay-4s" style="font-size: 12px">Acoustic Levitation Engineer</h3>' +
+'      </div>' +
+'    </div>';
