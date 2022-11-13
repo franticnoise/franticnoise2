fd31d72006ce2ff655f27e4fad0a9ea679631b0c
franticnoise <111320557+franticnoise@users.noreply.github.com>
2022-11-13 09:23:01 -0600

    11

--- footer.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('footer').innerHTML =
-  "<div id='footer'><br>" +
-  ' All rights reserved www.franticnoise.com 2022  ' +
-  '</div>';
--- header.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('header').innerHTML =
-'<div class="headerparent">' +
-'        <div class="headerchild">' +
-'          <img class="franticlogo" src="img/FRANTIC-NOISE-LOGO.png" width="300"/>' +
-//'        <h3 class="animate__animated animate__backInDown animate__delay-2s ">Frantic Noise </h3>' +
-//'        <h3 class="animate__animated animate__rubberBand animate__delay-4s" style="font-size: 17px">Acoustic Levitation Engineer</h3>' +
-'      </div>' +
-'    </div>';
