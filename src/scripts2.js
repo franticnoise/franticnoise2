12b14a88ecc91a7c11cc582ff6316848df1ee076
franticnoise <111320557+franticnoise@users.noreply.github.com>
2022-11-19 18:42:42 -0600

    1

--- src/scripts2.js
+++ src/scripts2.js
@@ -34,16 +34,17 @@ export function createHead(pagename) {
 
 ////////////// GUMROAD JSON PARSE 
 
-
-
-export async function loadJson(section) {
-
+export function gumroadScripts() { 
   const script = document.createElement("script");
   script.src = "https://assets.gumroad.com/packs/js/overlay-499efd442e6f54649ee6.js";
   script.charset = "utf-8";
   document.head.appendChild(script);
   document.head.innerHTML += '<link rel="stylesheet" href="https://assets.gumroad.com/packs/css/overlay-9325a7da.css" media="screen" />';
-  
+}
+
+export async function loadJson(section) {
+
+
   
   const res = await fetch("../src/gumroad.json");
   const json = await res.json();
